724702bf53d2d9b0cbd40cd29a00f6f4007abea2
Michelle Choi <michelle.choi@umusic.com>
2021-04-18 21:42:20 +0100

    spell check and brand logos

--- js/loader.js
+++ js/loader.js
@@ -1,9 +1,9 @@
-// Usage: <div id="filename">loading...</div><script src="loader.js"></script>
+// Usage: <div id="filename">loading...</div><script src="/js/loader.js"></script>
 // Must be no space between div and script
 
 function getPreviousSiblingOfThisScript() {
     var scripts = document.getElementsByTagName('script');
-    var thisScript = scripts[scripts.length-1];
+    var thisScript = scripts[scripts.length - 1];
     return thisScript.previousSibling;
 }
 
@@ -26,15 +26,15 @@ loadServerFileIntoElement(filename, target);
 $("#container").mousemove(function(e) {
     parallaxIt(e, ".slide", -100);
     parallaxIt(e, "img", -30);
-  });
-  
-  function parallaxIt(e, target, movement) {
+});
+
+function parallaxIt(e, target, movement) {
     var $this = $("#container");
     var relX = e.pageX - $this.offset().left;
     var relY = e.pageY - $this.offset().top;
-  
+
     TweenMax.to(target, 1, {
-      x: (relX - $this.width() / 2) / $this.width() * movement,
-      y: (relY - $this.height() / 2) / $this.height() * movement
+        x: (relX - $this.width() / 2) / $this.width() * movement,
+        y: (relY - $this.height() / 2) / $this.height() * movement
     });
-  }
+}
